e9c14f4432e1dd07f748fe62817911d3b372c7e2
An Pham <an.pham@gatech.edu>
2015-01-03 12:32:25 -0500

    Added more info

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  // response.send('Hello World!');
+  console.log('Connection established.');
   response.render('index.html');
+  console.log('Finshed page render.');
 });
 
 app.listen(app.get('port'), function() {
