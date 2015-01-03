517dd80337e8e5dc76a449280f158d5cd85984ab
An Pham <an.pham@gatech.edu>
2015-01-03 17:04:08 -0500

    Remove background on start

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -1,11 +1,12 @@
 
 tubeKaraApp
 .controller('MainCtrl', ['$scope', function($scope) {
-    $scope.welcome = 'Welcome to TubeKara 0.1.0! YouTube Karaoke is now on a different level.';
+    $scope.welcome = 'Welcome to TubeKara 0.1.0! YouTube Karaoke is now on a different level!...';
     $scope.list = [];
 
-    $scope.addSong = function(song) {
-      $scope.list.push(song);
+    $scope.start = function(song) {
+      $scope.list = [song];
+      $("html").css('background-image', 'none');
       console.log($scope.list);
     }
 }]);
