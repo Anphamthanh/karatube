f2936836aafae2d6e78ddcd0d1ed4cd34c1ec588
An Pham <an.pham@gatech.edu>
2015-01-03 18:24:11 -0500

    Get two column page

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -6,7 +6,13 @@ tubeKaraApp
 
     $scope.start = function(song) {
       $scope.list = [song];
+      height = $("html").height()*0.9;
       $("html").css('background-image', 'none');
+      $("#search-form").remove();
+      $("#player-frame").removeClass("hidden");
+      $("#player-frame").css("height", height);
+      $("#user-control").removeClass("hidden");
+      $("#user-control").css("height", height);
       console.log($scope.list);
     }
 }]);
