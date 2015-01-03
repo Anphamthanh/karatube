dbfa1c777056852942e73eaae82c306ec0345fba
An Pham <an.pham@gatech.edu>
2015-01-03 16:45:27 -0500

    Get song queueing works

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -2,4 +2,10 @@
 tubeKaraApp
 .controller('MainCtrl', ['$scope', function($scope) {
     $scope.welcome = 'Welcome to TubeKara 0.1.0! YouTube Karaoke is now on a different level.';
+    $scope.list = [];
+
+    $scope.addSong = function(song) {
+      $scope.list.push(song);
+      console.log($scope.list);
+    }
 }]);
